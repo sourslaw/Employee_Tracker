65d536c3a8caeebfd19cf42b07a72b3be99510cd
sourslaw <lawanscott@gmail.com>
2021-06-13 21:15:04 -0600

    updating sec proto

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
+require('dotenv').config();
+
 
 const connection = mysql.createConnection({
     host: 'localhost',
 
     port: 3306,
     // Your username
-    user: 'root',
-    password: 'Standard220',
-    database: 'emp_trackerDB',
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
 });
 
 
